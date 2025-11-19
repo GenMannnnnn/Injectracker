5561f13899ec4c755104e622d609ebecac312e69
qunli <qunli57.co@gmail.com>
2025-11-20 04:33:19 +0800

    新增多張照片編輯

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from .routers.vendors import router as vendors_router
 from .routers.products import router as products_router
 from .utils import UPLOAD_DIR
 
-BUILD_VERSION = "V2.1.1"
+BUILD_VERSION = "V2.2.0"
 
 app = FastAPI(title="ERP Mobile API")
 
